64f17b078fa5411b484d1aea73892bc5f8d8f07a
Will Fatherley <wfatherley@proton.me>
2025-02-18 09:29:44 -0500

    add os example to test-internals

--- tests/unit/test_internals.py
+++ tests/unit/test_internals.py
@@ -5,6 +5,44 @@ import unittest
 import exceptlib
 
 
+os_module_source_example = """
+def _execvpe(file, args, env=None):
+    if env is not None:
+        exec_func = execve
+        argrest = (args, env)
+    else:
+        exec_func = execv
+        argrest = (args,)
+        env = environ
+
+    if path.dirname(file):
+        exec_func(file, *argrest)
+        return
+    saved_exc = None
+    path_list = get_exec_path(env)
+    if name != 'nt':
+        file = fsencode(file)
+        path_list = map(fsencode, path_list)
+    for dir in path_list:
+        fullname = path.join(dir, file)
+        try:
+            exec_func(fullname, *argrest)
+        except (FileNotFoundError, NotADirectoryError) as e:
+            last_exc = e
+        except OSError as e:
+            last_exc = e
+            if saved_exc is None:
+                saved_exc = e
+    if saved_exc is not None:
+        raise saved_exc
+    raise last_exc
+""".strip()
+os_module_node = ast.parse(os_module_source_example)
+os_module_raise_nodes = [
+    n for n in ast.walk(os_module_node) if isinstance(n, ast.Raise)
+]
+
+
 dummy_module_source = """
 raise
 
